8f488084e13b040709c6d13acd6a5dcc458895c4
nikhilgirsa <nikhilgirsa@gmail.com>
2025-07-28 13:20:43 +0530

    Netflix-GPT

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+      <p>Welcome to my React application.</p>
+    </div>
+  );
+}
+
+export default App;
